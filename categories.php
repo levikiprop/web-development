08376d42a9002ed45fc649fcd6277abd0bfeeaf1
levikiprop <92566041+levikiprop@users.noreply.github.com>
2022-03-25 08:49:46 +0300

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,66 @@
+<?php include('partials-front/menu.php'); ?>
+
+
+
+<!-- CAtegories Section Starts Here -->
+<section class="categories">
+    <div class="container">
+        <h2 class="text-center">Explore Foods</h2>
+
+        <a href="category-foods.php">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="Pizza" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Pizza</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="Burger" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Burger</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/Blueberrypie.jpg" alt="Blueberry Pie" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Blueberry Pie</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/beefroll.jpeg" alt="Beef Roll" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Beef Roll</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/porknuggz.jpg" alt="Pork Nuggz" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Pork Nuggz</h3>
+            </div>
+        </a>
+
+        <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/steakbites.jpg" alt="Stake Bites" width="400" height="300" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Stake Bites</h3>
+            </div>
+        </a>
+
+
+
+
+        <div class="clearfix"></div>
+    </div>
+</section>
+<!-- Categories Section Ends Here -->
+
+<?php include('partials-front/footer.php'); ?>
---
+++ food-search.php
@@ -0,0 +1,135 @@
+<?php include('partials-front/menu.php'); ?>
+
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+  <div class="container">
+
+    <h2>Foods on Your Search <a href="#" class="text-white"></a></h2>
+
+  </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
+
+
+
+<!-- fOOD MEnu Section Starts Here -->
+<section class="food-menu">
+  <div class="container">
+    <h2 class="text-center">Food Menu</h2>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>Pizza</h4>
+        <p class="food-price">Ksh 1000</p>
+        <p class="food-detail">
+          A zesty main dish with a crisp, golden crust.
+        </p>
+        <br>
+
+        <a href="order.php" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>Smoky Burger</h4>
+        <p class="food-price">Ksh 350</p>
+        <p class="food-detail">
+          Made with Smokey Beef Patties.
+        </p>
+        <br>
+
+        <a href="#" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/beefroll.jpeg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>beef roll</h4>
+        <p class="food-price">Ksh 250</p>
+        <p class="food-detail">
+          Stuffed with a rainbow of fresh cilantro, tomato and scallion.
+        </p>
+        <br>
+
+        <a href="#" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/porknuggz.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>Pork Crispy Nuggz</h4>
+        <p class="food-price">$Ksh 700</p>
+        <p class="food-detail">
+          Made with fries, beef sausages, salsa, and pop sauce.
+        </p>
+        <br>
+
+        <a href="#" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/steakbites.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>Steakbites</h4>
+        <p class="food-price">Ksh 1200</p>
+        <p class="food-detail">
+          Made with fries, bacon, pop sauce and guacamole.
+        </p>
+        <br>
+
+        <a href="#" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+    <div class="food-menu-box">
+      <div class="food-menu-img">
+        <img src="images/buffalonuggz.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
+      </div>
+
+      <div class="food-menu-desc">
+        <h4>Buffalo nuggz</h4>
+        <p class="food-price">Ksh 1500</p>
+        <p class="food-detail">
+          Made with fries and jalapeno ranch sauce.
+        </p>
+        <br>
+
+        <a href="#" class="btn btn-primary">Order Now</a>
+      </div>
+    </div>
+
+
+    <div class="clearfix"></div>
+
+
+
+  </div>
+
+  <p class="text-center">
+    <a href="#">See All Foods</a>
+  </p>
+</section>
+<!-- fOOD Menu Section Ends Here -->
+
+<?php include('partials-front/footer.php'); ?>
